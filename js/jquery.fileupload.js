9bdfab6f98a1ab8ea65e820f90ced60faab41b4c
michaelbenin <michaellouisbenin@gmail.com>
2015-05-27 18:12:43 -0400

    webpack hotfix

--- js/jquery.fileupload.js
+++ js/jquery.fileupload.js
@@ -14,13 +14,7 @@
 
 (function (factory) {
     'use strict';
-    if (typeof define === 'function' && define.amd) {
-        // Register as an anonymous AMD module:
-        define([
-            'jquery',
-            'jquery.ui.widget'
-        ], factory);
-    } else if (typeof exports === 'object') {
+   if (typeof exports === 'object') {
         // Node/CommonJS:
         factory(
             require('jquery'),
